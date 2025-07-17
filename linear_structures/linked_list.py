4164944e16b843703d9e4501e45d2a431a0bbfc0
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-07-17 23:39:14 +0200

    linked_list.py algorithm

---
+++ linear_structures/linked_list.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+# Example
+ll = LinkedList()
+ll.add(1)
+ll.add(2)
+ll.print_list()  # Output: 2 -> 1 -> None
