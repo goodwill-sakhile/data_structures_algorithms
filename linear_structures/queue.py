75d708bb75455e405bfddf3826bf1b8326915385
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-07-17 23:37:34 +0200

    queue.py algorithm

---
+++ linear_structures/queue.py
@@ -0,0 +1,20 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.queue.pop(0)
+        return None
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+# Example
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+print(q.dequeue())  # Output: 1
