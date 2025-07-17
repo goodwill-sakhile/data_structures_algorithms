1e342704b56bacc1bb49d3b86927ef2b3f9c4ef3
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-07-17 23:42:14 +0200

    Create hash_table.py

---
+++ hash_structures/hash_table.py
@@ -0,0 +1,27 @@
+class HashTable:
+    def __init__(self):
+        self.size = 10
+        self.table = [[] for _ in range(self.size)]
+
+    def _hash(self, key):
+        return hash(key) % self.size
+
+    def insert(self, key, value):
+        index = self._hash(key)
+        for pair in self.table[index]:
+            if pair[0] == key:
+                pair[1] = value
+                return
+        self.table[index].append([key, value])
+
+    def get(self, key):
+        index = self._hash(key)
+        for pair in self.table[index]:
+            if pair[0] == key:
+                return pair[1]
+        return None
+
+# Example
+ht = HashTable()
+ht.insert("name", "Sakhile")
+print(ht.get("name"))  # Output: Sakhile
