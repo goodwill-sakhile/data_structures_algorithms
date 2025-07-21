ddbbd560d316bcd7e0e02749b9ac893113660bb3
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-07-21 23:44:07 +0200

    ConnectedComponents.cs Script

--- Data Structures in C#/GraphAlgorithms/ConnectedComponents.cs
+++ Data Structures in C#/GraphAlgorithms/ConnectedComponents.cs
@@ -1,2 +1,71 @@
 // ConnectedComponents implementation in C#
+using System;
+using System.Collections.Generic;
+
+class Graph
+{
+    private int Vertices;
+    private List<int>[] adjacencyList;
+
+    public Graph(int v)
+    {
+        Vertices = v;
+        adjacencyList = new List<int>[v];
+        for (int i = 0; i < v; i++)
+        {
+            adjacencyList[i] = new List<int>();
+        }
+    }
+
+    public void AddEdge(int u, int v)
+    {
+        adjacencyList[u].Add(v);
+        adjacencyList[v].Add(u); // Undirected graph
+    }
+
+    private void DFS(int v, bool[] visited, List<int> component)
+    {
+        visited[v] = true;
+        component.Add(v);
+
+        foreach (int neighbor in adjacencyList[v])
+        {
+            if (!visited[neighbor])
+            {
+                DFS(neighbor, visited, component);
+            }
+        }
+    }
+
+    public void FindConnectedComponents()
+    {
+        bool[] visited = new bool[Vertices];
+        int componentNumber = 1;
+
+        for (int v = 0; v < Vertices; v++)
+        {
+            if (!visited[v])
+            {
+                List<int> component = new List<int>();
+                DFS(v, visited, component);
+
+                Console.WriteLine($"Component {componentNumber}: {string.Join(", ", component)}");
+                componentNumber++;
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Graph g = new Graph(7);
+        g.AddEdge(0, 1);
+        g.AddEdge(0, 2);
+        g.AddEdge(3, 4);
+        g.AddEdge(5, 6);
+
+        Console.WriteLine("Connected Components:");
+        g.FindConnectedComponents();
+    }
+}
+
 
