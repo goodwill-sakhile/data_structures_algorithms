3a0b1ce0d1040f398dcf1e263aaacee824965af9
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-07-21 23:53:05 +0200

    Kruskal.cs Script

--- Data Structures in C#/GraphAlgorithms/Kruskal.cs
+++ Data Structures in C#/GraphAlgorithms/Kruskal.cs
@@ -1,2 +1,51 @@
 // Kruskal implementation in C#
+using System;
+using System.Collections.Generic;
+
+public class Edge : IComparable<Edge>
+{
+    public int Source, Destination, Weight;
+
+    public Edge(int source, int destination, int weight)
+    {
+        Source = source;
+        Destination = destination;
+        Weight = weight;
+    }
+
+    public int CompareTo(Edge other)
+    {
+        return this.Weight.CompareTo(other.Weight);
+    }
+}
+
+public class Subset
+{
+    public int Parent;
+    public int Rank;
+}
+
+public class Kruskal
+{
+    private int Vertices;
+    private List<Edge> Edges;
+
+    public Kruskal(int vertices)
+    {
+        Vertices = vertices;
+        Edges = new List<Edge>();
+    }
+
+    public void AddEdge(int source, int destination, int weight)
+    {
+        Edges.Add(new Edge(source, destination, weight));
+    }
+
+    // Find set of an element with path compression
+    private int Find(Subset[] subsets, int i)
+    {
+        if (subsets[i].Parent != i)
+            subsets[i].Parent = Find(subsets, subsets[i].Parent);
+
+        return subse
 
