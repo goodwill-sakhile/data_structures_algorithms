e732766538bf3aa4b0257209a5502522def0275c
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-07-22 13:03:45 +0200

    BinarySearchTreeOperations.cs Script

--- Data Structures in C#/TreeAlgorithms/BinarySearchTreeOperations.cs
+++ Data Structures in C#/TreeAlgorithms/BinarySearchTreeOperations.cs
@@ -1,2 +1,65 @@
 // BinarySearchTreeOperations implementation in C#
+using System;
+
+namespace BinarySearchTreeApp
+{
+    public class Node
+    {
+        public int Data;
+        public Node Left, Right;
+
+        public Node(int item)
+        {
+            Data = item;
+            Left = Right = null;
+        }
+    }
+
+    public class BinarySearchTree
+    {
+        public Node Root;
+
+        public BinarySearchTree()
+        {
+            Root = null;
+        }
+
+        // Insert operation
+        public void Insert(int data)
+        {
+            Root = InsertRec(Root, data);
+        }
+
+        private Node InsertRec(Node root, int data)
+        {
+            if (root == null)
+            {
+                root = new Node(data);
+                return root;
+            }
+
+            if (data < root.Data)
+                root.Left = InsertRec(root.Left, data);
+            else if (data > root.Data)
+                root.Right = InsertRec(root.Right, data);
+
+            return root;
+        }
+
+        // Search operation
+        public bool Search(int key)
+        {
+            return SearchRec(Root, key) != null;
+        }
+
+        private Node SearchRec(Node root, int key)
+        {
+            if (root == null || root.Data == key)
+                return root;
+
+            if (key < root.Data)
+                return SearchRec(root.Left, key);
+
+            return SearchRec(root.Right, key);
+
 
